9d9fc198010f2eacc72a62cc42bbaae732c129bd
d12fg12 <kuslyaka@gmail.com>
2022-04-22 21:57:49 -0400

    fixed errors

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class BinaryTree {
-    public boolean running;
-    Scanner scanner = new Scanner(System.in);
     TreeNode root;
-    BinaryTree t;
 
+    public BinaryTree() {
+        root = null;
+    }
 
     public boolean insert(Listing newStudent) {
         TreeNodeWrapper p = new TreeNodeWrapper();
@@ -46,7 +46,7 @@ public class BinaryTree {
         TreeNodeWrapper c = new TreeNodeWrapper();
         TreeNode largest, nextLargest;
         found = findNode(targetKey, p, c);
-        if (found == false) {
+        if (!found) {
             return false;
         } else {
             //case 1
@@ -94,9 +94,9 @@ public class BinaryTree {
     }
 
     public boolean update(String target, Listing l) {
-        if (delete(target) == false) {
+        if (!delete(target)) {
             return false;
-        } else if (insert(l) == false) {
+        } else if (!insert(l)) {
             return false;
         }
         return true;
@@ -164,81 +164,7 @@ public class BinaryTree {
     }
 
 
-    public void Menu() {
 
-        System.out.println("Enter: 1 to insert a new student's information");
-        System.out.println("2 to fetch and output a student's information");
-        System.out.println("3 to delete a student's information");
-        System.out.println("4 to update a student's information");
-        System.out.println("5 to output all student's information in descending order, and");
-        System.out.println("6 to exit the program.");
-        int choice = scanner.nextInt();
-
-        switch (choice) {
-            case 1:
-                System.out.println("Enter the student's full name: ");
-                String fullName = scanner.next();
-
-
-                System.out.println("Enter the student's id: ");
-                int id = scanner.nextInt();
-
-                System.out.println("Enter the student's gpa: ");
-                double gpa = scanner.nextDouble();
-                t.insert(new Listing(fullName, id, gpa));
-                break;
-
-            case 2:
-                System.out.println("Enter the student's full name: ");
-                fullName = scanner.nextLine();
-                System.out.println(t.fetch(fullName));
-                break;
-
-            case 3:
-                System.out.println("Enter the student's full name: ");
-                fullName = scanner.nextLine();
-
-                t.delete(fullName);
-                System.out.println(fullName+ "has been removed.");
-                break;
-            case 4:
-                System.out.println("Enter the student's full name: ");
-                fullName = scanner.nextLine();
-
-                System.out.println("Enter the student's new information at the following prompts:");
-                System.out.println("Update the student's name: ");
-                String newName = scanner.nextLine();
-
-                System.out.println("Update the student's id: ");
-                id = scanner.nextInt();
-
-                System.out.println("Update the student's gpa: ");
-                gpa = scanner.nextDouble();
-
-                t.update(fullName, new Listing(newName, id, gpa));
-
-                System.out.println(newName + " has been updated.");
-                break;
-            case 5:
-                t.RNLTraversal(t.root);
-                break;
-            case 6:
-                System.out.println("Are You Sure You Want To Exit? "
-                        +"Enter 'y' or 'Y' To Exit The Program Or Enter Any Other Key To Continue:");
-                String exit = scanner.nextLine();
-                if(exit.equalsIgnoreCase("y") )
-                {
-                    System.out.println("Exiting Program");
-                    running = false;
-                    System.exit(0);
-                }
-                else
-                {
-                    break;
-                }
-        }
-
-    }
 }
 
 
--- src/StudentInformation.java
+++ src/StudentInformation.java
@@ -5,6 +5,7 @@ public class StudentInformation {
     public static void main(String[] args) {
 
         BinaryTree t = new BinaryTree();
+        boolean exitProg = false;
 
 
         Listing student1 = new Listing("Garry Smith", 12345, 2.8);
@@ -18,10 +19,77 @@ public class StudentInformation {
         t.insert(student4);
         t.insert(student5);
 
+        while (!exitProg) {
+            Scanner scanner = new Scanner(System.in);
 
-        t.Menu();
+            System.out.println("Enter: 1 to insert a new student's information");
+            System.out.println("2 to fetch and output a student's information");
+            System.out.println("3 to delete a student's information");
+            System.out.println("4 to update a student's information");
+            System.out.println("5 to output all student's information in descending order, and");
+            System.out.println("6 to exit the program.");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the student's full name: ");
+                    String fullName = scanner.next();
 
 
+                    System.out.println("Enter the student's id: ");
+                    int id = scanner.nextInt();
 
+                    System.out.println("Enter the student's gpa: ");
+                    double gpa = scanner.nextDouble();
+                    t.insert(new Listing(fullName, id, gpa));
+                    break;
+
+                case 2:
+                    System.out.println("Enter the student's full name: ");
+                    fullName = scanner.nextLine();
+                    System.out.println(t.fetch(fullName));
+                    break;
+
+                case 3:
+                    System.out.println("Enter the student's full name: ");
+                    fullName = scanner.nextLine();
+                    t.delete(fullName);
+
+                    System.out.println(fullName + "has been removed.");
+                    break;
+                case 4:
+                    System.out.println("Enter the student's full name: ");
+                    fullName = scanner.nextLine();
+
+                    System.out.println("Enter the student's new information at the following prompts:");
+                    System.out.println("Update the student's name: ");
+                    String newName = scanner.nextLine();
+
+                    System.out.println("Update the student's id: ");
+                    id = scanner.nextInt();
+
+                    System.out.println("Update the student's gpa: ");
+                    gpa = scanner.nextDouble();
+
+                    t.update(fullName, new Listing(newName, id, gpa));
+
+                    System.out.println(newName + " has been updated.");
+                    break;
+                case 5:
+                    t.RNLTraversal(t.root);
+                    break;
+                case 6:
+                    System.out.println("Exiting Program");
+                    exitProg = true;
+                    break;
+                default:
+                    System.out.println("Choose a number");
+                    break;
+            }
+
+        }
     }
 }
+
+
+
